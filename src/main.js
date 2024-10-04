b0d3ecc44481dc295960b1a4f917869cb3b2d036
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-10-04 12:44:46 +0300

    add aos animations

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { CountUp } from 'countup.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
 
 window.onload = function () {
   const countUp1 = new CountUp('count1', 100);
